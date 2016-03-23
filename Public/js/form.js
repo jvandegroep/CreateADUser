c46969a52bae0f3e2bcc576db768ae5bf7d68e64
Jacob van de Groep <jaapgroep@hotmail.com>
2016-03-23 22:32:21 +0100

    add new form and script.js

---
+++ Public/js/form.js
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Execute funtions based on user key-events
+//
+$(document).ready(function(){
+    //hide elements
+    $("#previewField").hide();
+    $("#requestStatusCon").hide();
+
+    //execute function on keyup in form so user sees while typing.
+    $("input").change(function(){
+        PreviewUserInput();
+
+        //if initials element has a value check the username.
+        if (document.getElementById('initials').value) {
+          checkUserName();
+
+          //if element is checked check eu account.
+          if (document.getElementById('CreateEUaccount').checked) {
+            checkEUAccount();
+          }
+        }
+    });
+
+    //Show previewfield when the 'initials' input field is pressed.
+    $("#initials").change(function(){
+        $("#previewField").show("slow");
+    });
+
+    //Show statusrequestCon when submit button is clicked.
+    $("#button1").click(function(){
+        $("#requestStatusCon").show("slow");
+    });
+
+});
